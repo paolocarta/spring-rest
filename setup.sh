22fc426d04657216a02512c259b3817d3ce304a6
Paolo Carta <ing.pcarta@gmail.com>
2020-04-07 13:47:46 +0200

    adapting submodule folder name

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ sleep 1
 
 
 
-helm upgrade --install tekton-helm-pipeline ./tekton-pipeline -n basic-spring-boot-cicd --values values.yaml
+helm upgrade --install tekton-helm-pipeline ./tekton-pipe -n basic-spring-boot-cicd --values values.yaml
 
 sleep 1
 
 # start pipe
-oc apply -f ./tekton-pipeline/pipelinerun.yaml -n basic-spring-boot-cicd
+oc apply -f ./tekton-pipe/pipelinerun.yaml -n basic-spring-boot-cicd
 
 oc project basic-spring-boot-cicd
--- start-pipe.sh
+++ start-pipe.sh
@@ -4,4 +4,4 @@ oc delete pipelinerun basic-spring-boot-pipeline-run
 sleep 1
 
 
-oc apply -f ./tekton-pipeline/pipelinerun.yaml -n basic-spring-boot-cicd
+oc apply -f ./tekton-pipe/pipelinerun.yaml -n basic-spring-boot-cicd
