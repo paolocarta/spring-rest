bcbf4c27fe5639e72aaacfec62e52a4d02188013
Paolo Carta <ing.pcarta@gmail.com>
2020-04-08 18:18:45 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,11 @@
 #
 #
-oc apply -f tekton-pipeline/ocp-projects
+oc apply -f tekton-pipe/ocp-projects
 sleep 1
-
-
 
 
 helm upgrade --install tekton-helm-pipeline ./tekton-pipe -n basic-spring-boot-cicd --values values.yaml
 
 sleep 1
-
-# start pipe
-oc apply -f ./tekton-pipe/pipelinerun.yaml -n basic-spring-boot-cicd
 
 oc project basic-spring-boot-cicd
